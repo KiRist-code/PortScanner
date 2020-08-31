89f31e0d16944d9fbcc29e2f30ef1d9ab7a5d511
KiRist <37296174+KiRist-code@users.noreply.github.com>
2020-08-31 16:37:59 +0900

    Update port.py
    add UI

--- port.py
+++ port.py
@@ -1,15 +1,39 @@
+import pyfiglet
+import sys
 import socket
+from datetime import datetime
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(5)
+ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
+print(ascii_banner)
 
-host = input("Plz enter any IP u want to scan:")
-port = int(input("Plz enter any Port u want to scan:"))
+#defining a target 
+if len(sys.argv) == 2:
+    print("-" *50)
+    target = input("INPUT THE target ip:")
+else:
+    print("Invalid ammount of Argument")
+    
+#add banner
+print("-" * 50)
+print("Scanning Target: " + target)
+print("Scanning started at: " + str(datetime.now())
+print("-" * 50)
 
-def portScanner(port):
-    if s.connect_ex((host,port)):
-        print("The port is closed");
-    else:
-        print("The port is open")
-
-portScanner(port)
+try:
+    #Scanning port between 1 to 65535
+    for port in range(1,65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        
+        #returns an error indicator
+        result = s.conner_ex((target,port))
+        if result == 0:
+            print("Port {} is open",format(port))
+        s.close()
+        
+except KeyboardInteruppt:
+    print("\n Extting Program...")
+    sys.exit()
+except socket.error:
+    print("\n Server not responding...")
+    sys.exit()
